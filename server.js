43f28ea7963fac4e9495a646b6f2cc1669514c11
aphenaphen79-rgb <aphenaphen79@gmail.com>
2026-09-16 08:40:41 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 module.exports = function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1 style="text-align:center;margin-top:100px;font-family:sans-serif">🚀 Server Online! Berhasil!</h1><p style="text-align:center">Upgrade Required sudah hilang.</p>');
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  res.end('<h1 style="text-align:center;margin-top:100px;font-family:sans-serif">Server Online! Berhasil!</h1><p style="text-align:center">Upgrade Required sudah hilang.</p>');
 }
